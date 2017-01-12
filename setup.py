bad1ad1fc9b04cb7488489a772ca504519b6b998
mdiblasio <mdiblasio@google.com>
2017-01-12 11:16:41 -0500

    First Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/mdiblasio/funniest',
+      author='Michael DiBlasio',
+      author_email='flyingcircus@example.com',
+      license='dibs',
+      packages=['funniest'],
+      zip_safe=False)
